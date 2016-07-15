dd8c83fca6086c7b484d90954da4052d2ef490c6
John Wu <jwu@gracenote.com>
2016-07-15 15:13:17 +1000

    deal with indentation, changed diff-parse library, fix commit info

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var https = require('https');
 var crypto = require('crypto');
 var config = require('./config.json');
-var parse = require('parse-diff');
+var parse = require('diff-parse');
 var nodemailer = require('nodemailer');
 var escape = require('escape-html');
 
@@ -19,48 +19,65 @@ exports.handler = (event, context, callback) => {
         commits.forEach(function(e,i,a){
             getDiffFromGitHub(e.url)
             .then((diff)=>{
-                var files = parse(diff);
+                var files = parse(diff);                
                 var email = '<html><body>';
                 
                 email += 'Branch: ' + event['body-json'].ref + '<br/>';
                 email += 'Home: <a href="' + event['body-json'].repository.html_url + '">' + event['body-json'].repository.html_url + '</a><br/>';
-                email += 'Commit: <a href="' + event['body-json'].head_commit.url + '">' + event['body-json'].head_commit.id + '</a><br/>';
-                email += 'Author: <a href="mailto:' + event['body-json'].head_commit.author.email + '">' + event['body-json'].head_commit.author.name + '</a><br/>';
+                email += 'Commit: <a href="' + e.url + '">' + e.id + '</a><br/>';
+                email += 'Author: <a href="mailto:' + e.author.email + '">' + e.author.name + '</a><br/>';
                 email += 'Compare: <a href="' + event['body-json'].compare + '">' + event['body-json'].compare + '</a><br/>';
+                
+
                 files.forEach(function(file){
+                
                     email += '<hr>' + file.to + '</hr>'; //file modified name
                     email += '<table style="font-family: monospace, \'Courier New\', Courier; font-size: 12px; margin: 0;">';
 
-                    file.chunks.forEach(function(chunk){
-                        chunk.changes.forEach(function(change,idx,array){
-                            //Build HTML EMAIL
-                            if(change.add){
-                                email += '<tr style="background-color:#ddffdd;">';
+                    for(var i = 0; i < file.lines.length; i++){
+                        //Build HTML EMAIL
+                        var change = file.lines[i];
+
+                        if(change.add){
+                            email += '<tr style="background-color:#ddffdd;">';
+                        } else if (change.del){
+                            email += '<tr style="background-color:#ffdddd;">';
+                        } else if (change.normal){
+                            email += '<tr">';
+                        }                
+
+                        if(change.add || change.del || change.normal){
+                            email += '<td style="border-right:1px solid lightgray;">';
+                            email += (change.normal) ? change.ln2 : change.ln;
+                            email += '</td>';   
+                            //Deal with tabs and spaces for emails
+                            var content = escape(change.content);
+                            content = content.replace(/\t/g,'\u00a0 \u00a0'); 
+                            content = content.replace(/ /g, '\u00a0');   
+
+                            if(change.add === true){
+                                email += '<td>+' + content + '</td>';
                             } else if (change.del){
-                                email += '<tr style="background-color:#ffdddd;">';
-                            } else if (change.normal){
-                                email += '<tr">';
-                            }                         
-                                email += '<td style="border-right:1px solid lightgray;">';
-                                email += (change.normal) ? change.ln2 : change.ln;
-                                email += '</td>';
-                                
-                                email += '<td>' + escape(change.content) + '</td>';
-
+                                email += '<td>-' + content + '</td>';   
+                            } else {
+                                email += '<td>' + content + '</td>';   
+                            }
                             email += '</tr>';
-                        });              
-                    });
-
+                        }
+                            
+                        
+                    };
+                                   
                     email+='</table><br/>';
                 });
+
                 email+='</body></html>';
-                return Promise.resolve(email);
+                return Promise.resolve({email:email,commit:e});
             })
-            .then((email)=>{
-                var subject = '[' + event['body-json'].repository.full_name + '] ' + event['body-json'].head_commit.id.substring(0,6) + ': ' + event['body-json'].head_commit.message;
-                var from = event['body-json'].head_commit.author.name + '<' + event['body-json'].head_commit.author.email + '>';
-                console.log(email);
-                sendMail(email, subject, from);
+            .then((c)=>{
+                var subject = '[' + event['body-json'].repository.full_name + '] ' + c.commit.id.substring(0,6) + ': ' + c.commit.message;
+                var from = c.commit.author.name + '<' + c.commit.author.email + '>';                
+                sendMail(c.email, subject, from);
             })
             .catch((err)=>{
                 callback(err);
@@ -105,35 +122,6 @@ function sendMail(email, subject, from){
 
     var options = {
         host:config.mailConfig.host,
-        port: config.mailConfig.port
-    };
-    
-    var transporter = nodemailer.createTransport(options);
-
-    // setup e-mail data with unicode symbols
-    var mailOptions = {
-        from: from, 
-        to: config.mailConfig.to, // can be a command separated list of receivers
-        subject: subject, 
-        html: email 
-    };
-    
-    transporter.sendMail(mailOptions, function(error, info){
-        if(error) console.log(error);        
-        console.log('Message sent: ' + info.response);
-        transporter.close(); 
-    });
-};
-
-function sendMail(email, subject, from){ 
-
-    var auth = {
-        user: config.mailConfig.username,
-        pass: config.mailConfig.password
-    };
-
-    var options = {
-        host:config.mailConfig.host,
         port: config.mailConfig.port,
         secure: config.mailConfig.secure,
         requireTLS: config.mailConfig.requireTLS      
@@ -161,5 +149,4 @@ function sendMail(email, subject, from){
         transporter.close(); 
     });
 };
-
 
